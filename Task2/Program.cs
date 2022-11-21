084c263cd21a48c886de1b9d4e5991019c8d2f4f
Lera <lerizlot@icloud.com>
2022-11-21 19:19:47 +0400

    Реализован ввод чисел пользователем

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,49 +1,17 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
 // какое число большее, а какое меньшее.
 
-// a1 = 5; b1 = 7 -> max1 = 7
-// a2 = 2 b2 = 10 -> max2 = 10
-// a3 = -9 b3 = -3 -> max3 = -3
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
 
-// первый вариант
-int a1 = 5;
-int b1 = 7;
-int max1 = 0;
-
-if (a1 < b1) max1 = b1;
-if (a1 > b1) max1 = a1;
-
-Console.Write("max1 = ");
-Console.WriteLine(max1);
-
-int a2 = 2;
-int b2 = 10;
-int max2 = 0;
-
-if (a2 < b2) max2 = b2;
-if (a2 > b2) max2 = a2;
-
-Console.Write("max2 = ");
-Console.WriteLine(max2);
-
-int a3 = -9;
-int b3 = -3;
-int max3 = 0;
-
-if (a3 < b3) max3 = b3;
-if (a3 > b3) max3 = a3;
-
-Console.Write("max3 = ");
-Console.WriteLine(max3);
-
-// альтернативный вариант
 Console.WriteLine("Введите число a");
-int a0 = Convert.ToInt32(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число b");
-int b0 = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
 
-if (a0 < b0){
+if (a < b){
 Console.WriteLine("Max = ");
-Console.WriteLine(b0);
+Console.WriteLine(b);
 }
 
