c2180ad4eb246536449f7f4c4e97af9850f7ca6d
Lera <lerizlot@icloud.com>
2022-11-28 13:36:30 +0400

    Выполнена задача #23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -2,3 +2,9 @@
 
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите ваше число");
+int N = Convert.ToInt16(Console.ReadLine());
+
+for (int i = 1; i < N + 1; i++)
+    Console.WriteLine(i*i*i);
