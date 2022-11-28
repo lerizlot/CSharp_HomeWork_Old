6346b43406b01b420be167ef4ccffa637f7760d1
Lera <lerizlot@icloud.com>
2022-11-28 22:20:06 +0400

    Выполнена задача #21

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -2,3 +2,24 @@
 
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+/* Расстояние между точками — это число, равное корню из суммы квадратов разностей координат точек или длине разности радиус-векторов точек.
+d=sqrt(x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2);
+*/
+
+Console.WriteLine("Введите координату x1");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y1");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату z1");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату x2");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y2");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату z2");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double d = Math.Sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2));
+Console.WriteLine(Math.Round(d, 2));
