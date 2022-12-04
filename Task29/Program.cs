2fa65b174a2581b3eace20c9fd4cf0cd8b43b7c4
Lera <lerizlot@icloud.com>
2022-12-04 17:38:21 +0400

    Выполнена задача #29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -2,3 +2,11 @@
 
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+
+int n = 8;
+int[] num = new int [n];
+for (int i = 0; i < n; i++)
+{
+    num[i] = new Random().Next(0,100);
+    Console.Write($"{num[i]} ");
+}
